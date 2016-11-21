9049ae1619dddf4e1a1f82a4721933d26dba1822
RuihuiLim <RHLimz@gmail.com>
2016-11-21 12:58:54 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Hello World</title>
+
+        
+    </head>
+    <body>
+    <?php echo '<h1>Hello World, this is just a tryout.</h1>'; ?>
+
+    </body>
+</html>
